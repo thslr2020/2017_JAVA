e88780a4236ce9148a563f3472430717aec571a9
Misty <Misty@WiziSurf3>
2017-05-12 18:46:56 +0900

    어레이

--- Account/src/Exception.java
+++
@@ -1,6 +0,0 @@
-
-public class Exception extends java.lang.Exception {
-	public Exception(String msg) {
-		super(msg);
-	}
-}
--- Account/src/Valuable.java
+++
@@ -1,4 +0,0 @@
-public interface Valuable {
-	double estimateValue();
-	double estimateValue(int month);
-}
